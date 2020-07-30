978819c0916b8f370aac118dc06b9dc08a889d71
Abhilash CR <abhilash.cr@mountblue.tech>
2020-07-30 20:47:42 +0530

    modified the program such that user can enter the status code on url /status/{status-code}

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const fs=require("fs")
 //creating http server 
 http.createServer(function (req, res) {
     res.writeHead(200,{'Content-Type': 'text/plain'});
+    const pattern_status = RegExp('/status/[0-9]{3}');
+    const pattern_statusCode = RegExp('[0-9]{3}');
+    let str=req.url;
     if(req.url === "/html"){
         req.url = "/index.html";
         res.writeHead(200,{'Content-Type': 'text/html'});
@@ -13,5 +16,12 @@ http.createServer(function (req, res) {
         req.url = "/output.json";
         res.end(fs.readFileSync("."+req.url));
     }
+    else if(pattern_status.test(req.url)){ This is default page!!
+        req.statusCode=str.match(pattern_statusCode)[0];
+        res.writeHead(str.match(pattern_statusCode)[0]);
+        res.url="/"
+        res.write("The status code has been updated to "+str.match(pattern_statusCode)[0])
+        res.end();
+   }
     
 }).listen(8087)
