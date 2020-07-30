c545283d33c9f1d79876675b61171df93da9e391
Abhilash CR <abhilash.cr@mountblue.tech>
2020-07-30 20:45:14 +0530

    added condition to read /json file

--- server.js
+++ server.js
@@ -5,7 +5,12 @@ http.createServer(function (req, res) {
     res.writeHead(200,{'Content-Type': 'text/plain'});
     if(req.url === "/html"){
         req.url = "/index.html";
-        res.writeHead(203,{'Content-Type': 'text/html'});
+        res.writeHead(200,{'Content-Type': 'text/html'});
+        res.end(fs.readFileSync("."+req.url));
+    }
+    else if(req.url === "/json"){
+        res.writeHead(200,{'Content-Type': 'text/json'});
+        req.url = "/output.json";
         res.end(fs.readFileSync("."+req.url));
     }
     
