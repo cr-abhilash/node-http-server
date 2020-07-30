1636658de3c647a88281cd47873618ad00d6c3d2
Abhilash CR <abhilash.cr@mountblue.tech>
2020-07-30 20:44:15 +0530

    added condition to read /html file

--- server.js
+++ server.js
@@ -3,5 +3,10 @@ const fs=require("fs")
 //creating http server 
 http.createServer(function (req, res) {
     res.writeHead(200,{'Content-Type': 'text/plain'});
-    res.end(fs.readFileSync("readme.txt"))
+    if(req.url === "/html"){
+        req.url = "/index.html";
+        res.writeHead(203,{'Content-Type': 'text/html'});
+        res.end(fs.readFileSync("."+req.url));
+    }
+    
 }).listen(8087)
