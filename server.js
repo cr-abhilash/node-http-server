d2e4fc9764692fe19c1677feade2aaf645c8f9c2
Abhilash CR <abhilash.cr@mountblue.tech>
2020-07-30 20:41:47 +0530

    created http-server listining on port 8087

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
 const http=require("http")
 const fs=require("fs")
-
+//creating http server 
+http.createServer(function (req, res) {
+    res.writeHead(200,{'Content-Type': 'text/plain'});
+    res.end(fs.readFileSync("readme.txt"))
+}).listen(8087)
