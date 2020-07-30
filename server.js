9258f6553c391c2639c1e4b209d1e04fe0bbb068
Abhilash CR <abhilash.cr@mountblue.tech>
2020-07-30 20:57:34 +0530

    modified the program such that user can enter the delay to load the page  on url /delay/{delay in sec}

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ http.createServer(function (req, res) {
     res.writeHead(200,{'Content-Type': 'text/plain'});
     const pattern_status = RegExp('/status/[0-9]{3}');
     const pattern_statusCode = RegExp('[0-9]{3}');
+    const pattern_delay=  RegExp('/delay/[0-9]+');
+    const pattern_delayTime=  RegExp('[0-9]+');
     let str=req.url;
     if(req.url === "/html"){
         req.url = "/index.html";
@@ -16,12 +18,25 @@ http.createServer(function (req, res) {
         req.url = "/output.json";
         res.end(fs.readFileSync("."+req.url));
     }
-    else if(pattern_status.test(req.url)){ This is default page!!
+    else if(pattern_status.test(req.url)){ 
         req.statusCode=str.match(pattern_statusCode)[0];
         res.writeHead(str.match(pattern_statusCode)[0]);
         res.url="/"
         res.write("The status code has been updated to "+str.match(pattern_statusCode)[0])
         res.end();
-   }
+    }
+   else if(pattern_delay.test(req.url)){ 
+        res.url="/";
+        let delay=str.match(pattern_delayTime)[0];
+        let delayTime= new Promise((resolve,reject)=>setTimeout(()=>{
+        res.write("This page has been delayed by "+ delay + " seconds")
+        resolve();
+   
+        },delay*1000));
+        delayTime.then(()=>{res.end()}).catch((err)=>console.log(err))
+    }
+    else{
+        res.end(fs.readFileSync("./readme.txt"));
+    }
     
 }).listen(8087)
